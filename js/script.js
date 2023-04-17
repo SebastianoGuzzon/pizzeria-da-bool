e9eb25d505da7c8cfc696eb81848aab6f9c87eb6
SebastianoGuzzon <sebyguzz@icloud.com>
2023-04-17 10:22:10 +0200

    commit re-do exercise file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,58 @@
-import pizze from './db.js'
-import { checkVeg } from './functions.js';
 
+import pizze from './db.js';
+import {checkVeg} from './functions.js'
 
+const ul = document.querySelector('ul')
 
-const pizzeRemap = pizze.map( pizza => {
-  const {name, image, price, description, ingredients, votes} = pizza;
+const submit = document.getElementById('btnform');
+
+const noVegIngredients = ['salame', 'mozzarella', 'salsiccia', 'acciughe', 'prosciutto', 'grana', 'scamorza', 'gorgonzola'];
+
+const pizzeRemap = pizze.map(pizza => {
+  const {name, image, price, description, ingredients, votes} = pizza
   const isVeg = checkVeg(ingredients);
   return {name, image, price, description, ingredients, votes, isVeg}
-} )
+});
 
-pizzeRemap.sort((a,b) => a.price - b.price)
+//ordina pizze per prezzo crescente
 
-console.log(pizzeRemap);
+reorderPizze()
 
+submit.addEventListener('click', function(){
+  const inputName = document.querySelector('[name = "name"]');
+  const inputPrice = document.querySelector('[name = "price"]');
+  const selectIngredients = document.querySelector('[name = "ingredients"]');
+  const selectedIngredients = [];
 
-// Accetta come parametro l'array delle pizze, calcola la media dei voti di ogni pizza e restituisce la pizza con la media più alta 
-// function getPizzaTop() {
+  for(let ingredient of selectIngredients.options){
+    if(ingredient.selected){
+      selectedIngredients.push(ingredient.value)
+    }
+  }
+  console.log(inputName.value, inputPrice.value, selectedIngredients);
 
-// }
+  const newPizza = {
+    name: inputName.value,
+    price: inputPrice.value,
+    ingredients: selectedIngredients,
+  }
+  pizzeRemap.push(newPizza);
+  reorderPizze()
+})
+
+//ordina pizze,
+// ordina pizze per prezzo crescente,
+// stampa
+
+function reorderPizze() {
+
+  pizzeRemap.sort((a, b) => a.price - b.price);
+
+  ul.innerHTML = '';
+  pizzeRemap.forEach(pizza => {
+    const li = document.createElement('li')
+    li.innerHTML = `${pizza.name} €${pizza.price}`;
+    ul.appendChild(li);
+  })
+  console.log(pizzeRemap);
+}
